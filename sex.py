a783929ebb2ed9d0ea07bfcd5bd532a77cc18e1b
Christine's Man <but____why@blackhole.com>
2023-07-13 11:34:42 -0700

    added gyro punctuated rest periods during sexual activity

--- sex.py
+++ sex.py
@@ -25,7 +25,7 @@ class Sex(threading.Thread):
         # When arousal reaches some set level, I want to start incrementing the amount added to arousal
         # It will be slight, but since it will have no cap, eventually wife will throw an exception code O0OO00OOh
         self.multiplier = 1.0
-        self.multiplier_increment = 0.018
+        self.multiplier_increment = 0.016
 
         # keep track of Arousal not changing
         self.last_arousal = 0.0
@@ -48,7 +48,14 @@ class Sex(threading.Thread):
         self.arousal_near_orgasm = 0.80
         self.arousal_to_orgasm = 0.98
 
-        # after wife orgasms, I want to monitor the gyro a while. When it's dead for a while, assume we're done.
+        # I only want to hear, I'm gonna cum, one time, not five
+        # if this is False, wife has not said it.
+        # flips to True after it is said
+        # resets to False when O is over
+        self.im_gonna_cum_was_said = False
+
+        # after wife orgasms, I want to monitor the gyro a while.
+        # When it's this still, it means we're done or stopped
         self.gyro_deadzone_after_orgasm = 0.03
 
         # bringing the gyro short term jostled reading into the bedroom.
@@ -57,75 +64,84 @@ class Sex(threading.Thread):
         # So at max it will double the intensity of sounds, and at min (0.0) it will leave the intensity alone
         self.gyro_jackup_intensity_max = 0.45
 
+        # After wife has an orgasm, there should be a period where we can rest
+        # End the rest period when the gyro level is no longer at rest
+        # So this is what will do that
+        self.after_orgasm_rest = False
+
+        # During a rest period, need this amount of jostle to resume sex
+        self.gyro_deadzone_unrest = 0.1
+
     def run(self):
         log.sex.debug("Thread started.")
 
-        try:
-            while True:
-                # graceful shutdown
-                if SHARED_STATE.please_shut_down:
-                    break
+        while True:
+            # graceful shutdown
+            if SHARED_STATE.please_shut_down:
+                break
 
-                log.sex.debug(
-                    "SexualArousal = %.2f  Multiplier: %.2f",
-                    SHARED_STATE.sexual_arousal,
-                    self.multiplier,
-                )
-
-                # Has sex stopped for a while?
-                if SHARED_STATE.sexual_arousal == self.last_arousal:
-                    self.arousal_stagnant_count += 1
-                else:
-                    self.arousal_stagnant_count = 0
-                    self.last_arousal = SHARED_STATE.sexual_arousal
-
-                # If there's been no vagina hits for a period of time, we must be done, reset all
-                if self.arousal_stagnant_count >= self.seconds_to_reset:
-                    log.sex.info("Arousal stagnated and was reset")
-                    self.arousal_stagnant_count = 0
-                    SHARED_STATE.sexual_arousal = 0.0
-                    self.multiplier = 1.0
-
-                # if we are currently OOOOOO'ing, then I want to figure out when we're done, using the gyro
-                if SHARED_STATE.sexual_arousal > self.arousal_to_orgasm:
-                    log.sex.debug(
-                        "JostledShortTermLevel: %.2f", SHARED_STATE.jostled_level_short
-                    )
-
-                    # if we are not getting jostled that means we're done.
-                    # However, this one time I came inside her but she wasn't done, because she's so fucking hot,
-                    # and so I helped her get off with my finger, and then she just said "wow" and was done
-                    # because she wasn't getting jostled
-                    # so I added a condition to fix that, hope it works.
-                    if (
-                        SHARED_STATE.jostled_level_short
-                        < self.gyro_deadzone_after_orgasm
-                        and SHARED_STATE.sexual_arousal > 1.05
-                    ):
-                        log.sex.debug("Orgasm complete")
-                        SHARED_STATE.horny = 0.0
-                        SHARED_STATE.sexual_arousal = self.arousal_post_orgasm
-                        # self.Multiplier = 1.0   trying not resetting this (oh yeah baby, tried it and now it stays)
-                        breath.thread.queue_sound(
-                            from_collection="sex_done",
-                            play_ignore_speaking=True,
-                            priority=9,
-                        )
-
-                # If we're to a certain point, start incrementing to ensure wife will cum eventually with enough time
-                # Just Keep Fucking, Just Keep Fucking
-                elif SHARED_STATE.sexual_arousal > 0.2:
-                    self.multiplier += self.multiplier_increment
-
-                time.sleep(1)
-
-        # log exception in the main.log
-        except Exception as ex:
-            log.main.error(
-                "Thread died. {0} {1} {2}".format(
-                    ex.__class__, ex, log.format_tb(ex.__traceback__)
-                )
+            log.sex.debug(
+                "SexualArousal = %.2f  Multiplier: %.2f",
+                SHARED_STATE.sexual_arousal,
+                self.multiplier,
             )
+
+            # Has sex stopped for a while?
+            if SHARED_STATE.sexual_arousal == self.last_arousal:
+                self.arousal_stagnant_count += 1
+            else:
+                self.arousal_stagnant_count = 0
+                self.last_arousal = SHARED_STATE.sexual_arousal
+
+            # If there's been no vagina hits for a period of time, we must be done, reset all
+            if self.arousal_stagnant_count >= self.seconds_to_reset:
+                log.sex.info("Arousal stagnated and was reset")
+                self.arousal_stagnant_count = 0
+                SHARED_STATE.sexual_arousal = 0.0
+                self.multiplier = 1.0
+                self.after_orgasm_rest = False
+                self.im_gonna_cum_was_said = False
+
+            # if we are currently OOOOOO'ing, then I want to figure out when we're done, using the gyro
+            if SHARED_STATE.sexual_arousal > self.arousal_to_orgasm:
+                log.sex.debug(
+                    "JostledShortTermLevel: %.2f", SHARED_STATE.jostled_level_short
+                )
+
+                # if we are not getting jostled that means we're done.
+                # However, this one time I came inside her but she wasn't done, because she's so fucking hot,
+                # and so I helped her get off with my finger, and then she just said "wow" and was done
+                # because she wasn't getting jostled
+                # so I added a condition to fix that, hope it works.
+                if (
+                    SHARED_STATE.jostled_level_short
+                    < self.gyro_deadzone_after_orgasm
+                    and SHARED_STATE.sexual_arousal > 1.05
+                ):
+                    log.sex.debug("Orgasm complete")
+                    SHARED_STATE.horny = 0.0
+                    SHARED_STATE.sexual_arousal = self.arousal_post_orgasm
+                    # self.Multiplier = 1.0   trying not resetting this (oh yeah baby, tried it and now it stays)
+                    breath.thread.queue_sound(
+                        from_collection="sex_done",
+                        play_ignore_speaking=True,
+                        priority=9,
+                    )
+                    self.after_orgasm_rest = True
+                    self.im_gonna_cum_was_said = False
+
+            # If we're to a certain point, start incrementing to ensure wife will cum eventually with enough time
+            # Just Keep Fucking, Just Keep Fucking
+            elif SHARED_STATE.sexual_arousal > 0.2:
+                self.multiplier += self.multiplier_increment
+
+            # if we're resting and the gyro starts to feel it, start again
+            # When after_orgasm_rest is True, we are ignoring vagina action
+            if self.after_orgasm_rest is True and SHARED_STATE.jostled_level_short > self.gyro_deadzone_unrest:
+                self.after_orgasm_rest = False
+
+            time.sleep(1)
+
 
     def vagina_got_hit(self, sensor_data):
         """
@@ -134,7 +150,7 @@ class Sex(threading.Thread):
         total capacitance of the circuit, resulting in a
         signal to the inorganic machine and a response.
         """
-        if SHARED_STATE.shush_please_honey is False:
+        if SHARED_STATE.shush_please_honey is False and self.after_orgasm_rest is False:
             # Stay awake
             sleep.thread.wake_up(0.001)
 
@@ -166,13 +182,15 @@ class Sex(threading.Thread):
                         play_no_wait=True,
                         priority=8,
                     )
-                elif SHARED_STATE.sexual_arousal > self.arousal_near_orgasm:
+                elif SHARED_STATE.sexual_arousal > self.arousal_near_orgasm and self.im_gonna_cum_was_said is False:
+                    # this should happen only one time per fuck cycle
                     breath.thread.queue_sound(
                         from_collection="sex_near_O",
                         play_ignore_speaking=True,
-                        play_no_wait=True,
-                        priority=8,
-                    )  # why was this here CutAllSoundAndPlay=True, oh, that's why, duh
+                        play_no_wait=False,
+                        priority=9,
+                    )
+                    self.im_gonna_cum_was_said = True
                 else:
                     # 92% chance of a regular sex sound, 8% chance of something sexy with words
                     if random.random() > 0.92:
