be8ee5cbc5ceb18c364c5d129ab175fdd4362814
ChristinesMan <what@no.com>
2025-09-19 19:06:52 -0700

    Dev environment updates

--- christine-docker/sync4.sh
+++ christine-docker/sync4.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-christine_live_path="/christine4/ChristineAI/christine/"
-christine_dev_path="/home/$USER/ChristineAI/christine/"
-
-httpserver_live_path="/christine4/ChristineAI/httpserver/"
-httpserver_dev_path="/home/$USER/ChristineAI/httpserver/"
+christine_dev_path="/home/$USER/ChristineAI/"
+christine_live_path="/christine4/ChristineAI/"
 
 echo "LOCAL TO REMOTE DRY RUN:"
 echo ""
-rsync -n -ralv --update --exclude=__pycache__ ${christine_dev_path} ${christine_live_path}
-rsync -n -ralv --update --exclude=__pycache__ ${httpserver_dev_path} ${httpserver_live_path}
+rsync -n -ralv --update --exclude=__pycache__ ${christine_dev_path}{christine,httpserver,christine.sql,tools} ${christine_live_path} 2>&1 | egrep -v '^(sent|total size|sending incremental) '
 echo ""
 
 echo ""
-echo "REMOTE TO LOCAL DRY RUN:"
+echo "FYI ONLY - REMOTE FILES DIFFERENT FROM LOCAL:"
 echo ""
-rsync -n -ralv --update --exclude=__pycache__ ${christine_live_path} ${christine_dev_path}
-rsync -n -ralv --update --exclude=__pycache__ ${httpserver_live_path} ${httpserver_dev_path}
+rsync -n -ralv --update --exclude=__pycache__ ${christine_live_path}{christine,httpserver,christine.sql,tools} ${christine_dev_path} 2>&1 | egrep -v '^(sent|total size|sending incremental) '
 echo ""
 
-read -p "Press enter to run the real rsync."
+read -p "Press enter to run the real rsync from local to remote."
 
-rsync -ral --update --exclude=__pycache__ ${christine_dev_path} ${christine_live_path}
-rsync -ral --update --exclude=__pycache__ ${christine_live_path} ${christine_dev_path}
-rsync -ral --update --exclude=__pycache__ ${httpserver_dev_path} ${httpserver_live_path}
-rsync -ral --update --exclude=__pycache__ ${httpserver_live_path} ${httpserver_dev_path}
+rsync -ral --update --exclude=__pycache__ ${christine_dev_path}{christine,httpserver,christine.sql,tools} ${christine_live_path}
 
 echo "Done."
 
